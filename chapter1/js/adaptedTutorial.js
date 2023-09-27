b1f619457fd79476cd246feadb297927e5277c56
maya-edstrom <medstrom@wisc.edu>
2023-09-26 23:45:58 -0500

    updated code, uploaded data
    updated code to get maps to load in html; added new data

---
+++ chapter1/js/adaptedTutorial.js
@@ -0,0 +1,32 @@
+var map;
+//function to instantiate the Leaflet map
+function createMap(){
+    //create the map
+    map = L.map('mapid', {
+        center: [20, 0],
+        zoom: 2
+    });
+
+    //add OSM base tilelayer
+    L.tileLayer('http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+        attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap contributors</a>'
+    }).addTo(map);
+
+    //call getData function
+    getData();
+};
+
+//function to retrieve the data and place it on the map
+function getData(){
+    //load the data
+    fetch("data/bigcities.geojson")
+        .then(function(response){
+            return response.json();
+        })
+        .then(function(json){
+            //create a Leaflet GeoJSON layer and add it to the map
+            L.geoJson(json).addTo(map);
+        })
+};
+
+document.addEventListener('DOMContentLoaded',createMap)
--- chapter1/js/main.js
+++ chapter1/js/main.js
@@ -1,1 +1,8 @@
 // Add all scripts to the JS folder
+var mymap = L.map('mapid').setView([51.505, -0.09], 13);
+
+// Add a tile layer to the map
+L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    maxZoom: 19,
+    attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+}).addTo(mymap);
