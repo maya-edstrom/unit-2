38a41a435ea53efcec54c89ae0cfe9b4ffcf9073
maya-edstrom <medstrom@wisc.edu>
2023-10-01 16:26:39 -0500

    finalized data and completed script for main.js

--- chapter1/js/main.js
+++ chapter1/js/main.js
@@ -1,8 +1,34 @@
-// Add all scripts to the JS folder
-var mymap = L.map('mapid').setView([51.505, -0.09], 13);
+var map;
+//function to instantiate the Leaflet map
+function createMap(){
+    //create the map
+    map = L.map('mapid', {
+        center: [20, 0],
+        zoom: 3
 
-// Add a tile layer to the map
-L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-    maxZoom: 19,
-    attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
-}).addTo(mymap);
+    });
+
+    //add OSM base tilelayer
+    L.tileLayer('http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+        attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap contributors</a>'
+    }).addTo(map);
+
+    //call getData function
+    getData();
+};
+
+
+//function to retrieve the data and place it on the map
+function getData(){
+    //load the data
+    fetch("data/AvgComTimesGJSON.geojson")
+        .then(function(response){
+            return response.json();
+        })
+        .then(function(json){
+            //create a Leaflet GeoJSON layer and add it to the map
+            L.geoJson(json).addTo(map);
+        })
+};
+
+document.addEventListener('DOMContentLoaded',createMap)
