445bde9a4c727d12179e3e73e375aa82dbcfcc30
maya-edstrom <medstrom@wisc.edu>
2023-10-05 23:07:41 -0500

    creating proportional symbols for time change map

--- chapter1/js/main.js
+++ chapter1/js/main.js
@@ -1,11 +1,14 @@
-var map;
-//function to instantiate the Leaflet map
+//declare map variable globally so all functions have access
+let map;
+let minValue;
+
+//step 1 create map
 function createMap(){
+
     //create the map
-    map = L.map('mapid', {
+    map = L.map('map', {
         center: [20, 0],
         zoom: 3
-
     });
 
     //add OSM base tilelayer
@@ -14,11 +17,70 @@ function createMap(){
     }).addTo(map);
 
     //call getData function
-    getData();
+    getData(map);
+};
+
+function calculateMinValue(data){
+    //create empty array to store all data values
+    let allValues = [];
+    //loop through each city
+    for(let city of data.features){
+        //loop through each year
+        for(let year = 2010; year <= 2019; year+=5){
+            //get population for current year
+            let value = city.properties["Pop_"+ String(year)];
+            //add value to array
+            allValues.push(value);
+        }
+    }
+    //get minimum value of our array
+    let minValue = Math.min(...allValues)
+
+    return minValue;
+}
+
+//calculate the radius of each proportional symbol
+function calcPropRadius(attValue) {
+    //constant factor adjusts symbol sizes evenly
+   let minRadius = 5;
+    //Flannery Appearance Compensation formula
+    let radius = 1.0083 * Math.pow(attValue/minValue,0.5715) * minRadius
+
+    return radius;
+};
+
+//Step 3: Add circle markers for point features to the map
+function createPropSymbols(data){
+
+    //Step 4: Determine which attribute to visualize with proportional symbols
+    let attribute = "AvgComTime2010";
+
+    //create marker options
+    let geojsonMarkerOptions = {
+        fillColor: "#ff7800",
+        color: "#fff",
+        weight: 1,
+        opacity: 1,
+        fillOpacity: 0.8,
+        radius: 8
+    };
+
+    L.geoJson(data, {
+        pointToLayer: function (feature, latlng) {
+            //Step 5: For each feature, determine its value for the selected attribute
+            var attValue = Number(feature.properties[attribute]);
+
+            //Step 6: Give each feature's circle marker a radius based on its attribute value
+            geojsonMarkerOptions.radius = calcPropRadius(attValue);
+
+            //create circle markers
+            return L.circleMarker(latlng, geojsonMarkerOptions);
+        }
+    }).addTo(map);
 };
 
 
-//function to retrieve the data and place it on the map
+//Step 2: Import GeoJSON data
 function getData(){
     //load the data
     fetch("data/AvgComTimesGJSON.geojson")
@@ -26,8 +88,10 @@ function getData(){
             return response.json();
         })
         .then(function(json){
-            //create a Leaflet GeoJSON layer and add it to the map
-            L.geoJson(json).addTo(map);
+            //calculate minimum data value
+            minValue = calculateMinValue(json);
+            //call function to create proportional symbols
+            createPropSymbols(json);
         })
 };
 
